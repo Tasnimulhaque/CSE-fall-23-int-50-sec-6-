4be6e98e5318a3e4c4b7295a00a1044b6af09288
Tasnimulhaque <122193986+Tasnimulhaque@users.noreply.github.com>
2023-01-08 23:59:50 +0600

    Add files via upload

---
+++ lab1/1.180-50-6.cpp
@@ -0,0 +1,14 @@
+//codeforce problem no:1772A
+//roblem name:A+b?
+#include <iostream>
+using namespace std;
+int main() {
+    int x,y,t;
+    char s;
+    cin>>t;
+    while(t--){
+       cin>>x>>s>>y;
+       cout<<x+y<<endl;
+    }
+    return 0;
+}
